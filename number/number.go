4ede0c389fb0fa5d9e489695c46092ebfb7042b6
chaozgz <charlesklient@gmail.com>
2022-04-02 00:51:57 +1100

    save

--- number/number.go
+++ number/number.go
@@ -8,3 +8,11 @@ func RoundT4(number float64) float64 {
 func RoundT2(number float64) float64 {
 	return math.Round(number*100) / 100
 }
+
+func FloorT2(number float64) float64 {
+	return math.Floor(number*100) / 100
+}
+
+func CeilT2(number float64) float64 {
+	return math.Ceil(number*100) / 100
+}
